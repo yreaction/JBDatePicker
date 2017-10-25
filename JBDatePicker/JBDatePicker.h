a27170f5c8af1e5d771907ffec9a917e718005db
Joe Burgess <errortype520@gmail.com>
2017-10-25 17:11:33 -0400

    No Message

--- JBDatePicker/JBDatePicker.h
+++ JBDatePicker/JBDatePicker.h
@@ -12,7 +12,7 @@
 FOUNDATION_EXPORT double JBDatePickerVersionNumber;
 
 //! Project version string for JBDatePicker.
-FOUNDATION_EXPORT const unsigned char JBDatePickerVersionString[];
+FOUNDATION_EXPORT const unsigned char JBDatePickerVersionString[]; 
 
 // In this header, you should import all the public headers of your framework using statements like #import <JBDatePicker/PublicHeader.h>
 
